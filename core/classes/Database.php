dea34cec7bc36d1abdac85babe403ffde8d1fced
guilhermeCBraz <lgcb@ic.ufal.br>
2023-01-27 17:17:18 -0300

    Second commit

--- core/classes/Database.php
+++ core/classes/Database.php
@@ -2,10 +2,27 @@
 
 namespace core\classes;
 
+use PDO;
+
 class Database
 {
-    public function __construct()
+    private $ligacao;
+
+    private function ligar()
     {
-        echo 'Base de dados';
+        $this->ligacao = new PDO(
+            'mysql:' .
+                'host=' . MYSQL_SERVER . ';' .
+                'dbname=' . MYSQL_DATABASE . ';' .
+                'charset=' . MYSQL_CHARSET,
+        );
     }
 }
+
+/*
+define('MYSQL_SERVER',      'localhost');
+define('MYSQL_DATABASE',    'php_store');
+define('MYSQL_USER',        'user_php_store');
+define('MYSQL_PASS',        '');
+define('MYSQL_CHARSET',     'utf-8');
+*/
--- public/index.php
+++ public/index.php
@@ -1,10 +1,5 @@
 <?php
-
-use core\classes\Database;
-use core\classes\Functions;
-
 // abrir a sessao
-
 session_start();
 
 // carregar o config
@@ -12,14 +7,6 @@ require_once('../config.php');
 
 // carrega todas as classes do projeto
 require_once('../vendor/autoload.php');
-
-$a = new Database();
-$b = new Functions();
-$b->teste();
-
-echo 'OK';
-
-// 
 
 /*
 carregar o config
